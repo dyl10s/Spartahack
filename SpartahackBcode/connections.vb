f72d4c7924af3799e9a0a388e6c7c297a72c5c60
Dylan Strohschein <dylanstrohschein@gmail.com>
2018-01-20 09:30:22 -0500

    Added sign out

---
+++ SpartahackBcode/connections.vb
@@ -0,0 +1,3 @@
+﻿Public Class connections
+
+End Class
--- SpartahackBcode/frmMain.Designer.vb
+++ SpartahackBcode/frmMain.Designer.vb
@@ -22,8 +22,29 @@ Partial Class frmMain
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.btnSignOut = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnSignOut
+        '
+        Me.btnSignOut.Location = New System.Drawing.Point(12, 12)
+        Me.btnSignOut.Name = "btnSignOut"
+        Me.btnSignOut.Size = New System.Drawing.Size(75, 23)
+        Me.btnSignOut.TabIndex = 0
+        Me.btnSignOut.Text = "Sign Out"
+        Me.btnSignOut.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(893, 443)
+        Me.Controls.Add(Me.btnSignOut)
+        Me.Name = "frmMain"
         Me.Text = "frmMain"
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents btnSignOut As Button
 End Class
