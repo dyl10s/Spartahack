987e0c321b5198f8779288dfdfa6ba952a7ab7d0
Adam Chlebek <adamchlebek@live.com>
2018-01-20 09:40:19 -0500

    Code implementation and Back Color

--- SpartahackBcode/frmLoginUpdated.Designer.vb
+++ SpartahackBcode/frmLoginUpdated.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class frmLoginUpdated
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,14 @@ Partial Class frmLoginUpdated
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.BunifuElipse1 = New Bunifu.Framework.UI.BunifuElipse(Me.components)
-        Me.txtUser = New Bunifu.Framework.UI.BunifuMetroTextbox()
+        Me.txtEmail = New Bunifu.Framework.UI.BunifuMetroTextbox()
+        Me.txtPassword = New Bunifu.Framework.UI.BunifuMetroTextbox()
+        Me.btnLogin = New Bunifu.Framework.UI.BunifuFlatButton()
+        Me.btnRegister = New Bunifu.Framework.UI.BunifuFlatButton()
         Me.SuspendLayout()
         '
         'BunifuElipse1
@@ -32,30 +35,118 @@ Partial Class frmLoginUpdated
         Me.BunifuElipse1.ElipseRadius = 5
         Me.BunifuElipse1.TargetControl = Me
         '
-        'txtUser
+        'txtEmail
         '
-        Me.txtUser.BorderColorFocused = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
-        Me.txtUser.BorderColorIdle = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
-        Me.txtUser.BorderColorMouseHover = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
-        Me.txtUser.BorderThickness = 3
-        Me.txtUser.Cursor = System.Windows.Forms.Cursors.IBeam
-        Me.txtUser.Font = New System.Drawing.Font("Century Gothic", 9.75!)
-        Me.txtUser.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
-        Me.txtUser.isPassword = False
-        Me.txtUser.Location = New System.Drawing.Point(23, 167)
-        Me.txtUser.Margin = New System.Windows.Forms.Padding(4)
-        Me.txtUser.Name = "txtUser"
-        Me.txtUser.Size = New System.Drawing.Size(370, 44)
-        Me.txtUser.TabIndex = 0
-        Me.txtUser.Text = "Username"
-        Me.txtUser.TextAlign = System.Windows.Forms.HorizontalAlignment.Left
+        Me.txtEmail.BorderColorFocused = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.txtEmail.BorderColorIdle = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.txtEmail.BorderColorMouseHover = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.txtEmail.BorderThickness = 3
+        Me.txtEmail.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.txtEmail.Font = New System.Drawing.Font("Cooper Black", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtEmail.ForeColor = System.Drawing.Color.Transparent
+        Me.txtEmail.isPassword = False
+        Me.txtEmail.Location = New System.Drawing.Point(56, 153)
+        Me.txtEmail.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(293, 54)
+        Me.txtEmail.TabIndex = 1
+        Me.txtEmail.TextAlign = System.Windows.Forms.HorizontalAlignment.Left
+        '
+        'txtPassword
+        '
+        Me.txtPassword.BorderColorFocused = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.txtPassword.BorderColorIdle = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.txtPassword.BorderColorMouseHover = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.txtPassword.BorderThickness = 3
+        Me.txtPassword.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.txtPassword.Font = New System.Drawing.Font("Cooper Black", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPassword.ForeColor = System.Drawing.Color.Transparent
+        Me.txtPassword.isPassword = True
+        Me.txtPassword.Location = New System.Drawing.Point(56, 245)
+        Me.txtPassword.Margin = New System.Windows.Forms.Padding(6, 5, 6, 5)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(293, 54)
+        Me.txtPassword.TabIndex = 2
+        Me.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Left
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Activecolor = System.Drawing.Color.Gray
+        Me.btnLogin.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.btnLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnLogin.BorderRadius = 0
+        Me.btnLogin.ButtonText = "Login"
+        Me.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnLogin.DisabledColor = System.Drawing.Color.Gray
+        Me.btnLogin.Iconcolor = System.Drawing.Color.Transparent
+        Me.btnLogin.Iconimage = Nothing
+        Me.btnLogin.Iconimage_right = Nothing
+        Me.btnLogin.Iconimage_right_Selected = Nothing
+        Me.btnLogin.Iconimage_Selected = Nothing
+        Me.btnLogin.IconMarginLeft = 0
+        Me.btnLogin.IconMarginRight = 0
+        Me.btnLogin.IconRightVisible = True
+        Me.btnLogin.IconRightZoom = 0R
+        Me.btnLogin.IconVisible = True
+        Me.btnLogin.IconZoom = 90.0R
+        Me.btnLogin.IsTab = False
+        Me.btnLogin.Location = New System.Drawing.Point(78, 345)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Normalcolor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.btnLogin.OnHovercolor = System.Drawing.Color.Silver
+        Me.btnLogin.OnHoverTextColor = System.Drawing.Color.White
+        Me.btnLogin.selected = False
+        Me.btnLogin.Size = New System.Drawing.Size(241, 48)
+        Me.btnLogin.TabIndex = 3
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.btnLogin.Textcolor = System.Drawing.Color.White
+        Me.btnLogin.TextFont = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        '
+        'btnRegister
+        '
+        Me.btnRegister.Activecolor = System.Drawing.Color.Gray
+        Me.btnRegister.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.btnRegister.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnRegister.BorderRadius = 0
+        Me.btnRegister.ButtonText = "Register"
+        Me.btnRegister.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnRegister.DisabledColor = System.Drawing.Color.Gray
+        Me.btnRegister.Iconcolor = System.Drawing.Color.Transparent
+        Me.btnRegister.Iconimage = Nothing
+        Me.btnRegister.Iconimage_right = Nothing
+        Me.btnRegister.Iconimage_right_Selected = Nothing
+        Me.btnRegister.Iconimage_Selected = Nothing
+        Me.btnRegister.IconMarginLeft = 0
+        Me.btnRegister.IconMarginRight = 0
+        Me.btnRegister.IconRightVisible = True
+        Me.btnRegister.IconRightZoom = 0R
+        Me.btnRegister.IconVisible = True
+        Me.btnRegister.IconZoom = 90.0R
+        Me.btnRegister.IsTab = False
+        Me.btnRegister.Location = New System.Drawing.Point(78, 410)
+        Me.btnRegister.Name = "btnRegister"
+        Me.btnRegister.Normalcolor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.btnRegister.OnHovercolor = System.Drawing.Color.Silver
+        Me.btnRegister.OnHoverTextColor = System.Drawing.Color.White
+        Me.btnRegister.selected = False
+        Me.btnRegister.Size = New System.Drawing.Size(241, 48)
+        Me.btnRegister.TabIndex = 4
+        Me.btnRegister.Text = "Register"
+        Me.btnRegister.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.btnRegister.Textcolor = System.Drawing.Color.White
+        Me.btnRegister.TextFont = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         '
         'frmLoginUpdated
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
         Me.ClientSize = New System.Drawing.Size(397, 511)
-        Me.Controls.Add(Me.txtUser)
+        Me.Controls.Add(Me.btnRegister)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtEmail)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "frmLoginUpdated"
         Me.Text = "frmLoginUpdated"
@@ -64,5 +155,8 @@ Partial Class frmLoginUpdated
     End Sub
 
     Friend WithEvents BunifuElipse1 As Bunifu.Framework.UI.BunifuElipse
-    Friend WithEvents txtUser As Bunifu.Framework.UI.BunifuMetroTextbox
+    Friend WithEvents txtEmail As Bunifu.Framework.UI.BunifuMetroTextbox
+    Friend WithEvents txtPassword As Bunifu.Framework.UI.BunifuMetroTextbox
+    Friend WithEvents btnLogin As Bunifu.Framework.UI.BunifuFlatButton
+    Friend WithEvents btnRegister As Bunifu.Framework.UI.BunifuFlatButton
 End Class
--- SpartahackBcode/frmLoginUpdated.vb
+++ SpartahackBcode/frmLoginUpdated.vb
@@ -1,3 +1,35 @@
 ﻿Public Class frmLoginUpdated
 
+    Private Sub frmLoginUpdated_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.BackColor = ColorTranslator.FromHtml("#2b627a")
+    End Sub
+
+    Private Sub txtUser_OnValueChanged(sender As Object, e As EventArgs) Handles txtEmail.OnValueChanged
+
+    End Sub
+
+    Private Sub BunifuFlatButton2_Click(sender As Object, e As EventArgs) Handles btnRegister.Click
+        Me.Hide()
+        Dim frmReg As New frmRegister
+        frmReg.Show()
+    End Sub
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        Dim sqlCon As New sqlManager
+
+        Try
+            Dim rawData As String = sqlCon.getData("Select fName, lName, Email from users where email = " + txtEmail.Text + " and password = " + txtPassword.Text, 3)
+
+            Dim fName As String = rawData.Split("~")(0)
+            Dim lName As String = rawData.Split("~")(1)
+            Dim email As String = rawData.Split("~")(2)
+
+            Dim clsUser As New clsUser(fName, lName, email)
+
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
 End Class
