6384986b4b52ad75c6cd142559b33de7bca9036c
Adam Chlebek <adamchlebek@live.com>
2018-01-20 09:21:06 -0500

    Update

--- SpartahackBcode/frmLoginUpdated.vb
+++ SpartahackBcode/frmLoginUpdated.vb
@@ -1,9 +1,3 @@
 ﻿Public Class frmLoginUpdated
-    Private Sub frmLoginUpdated_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-    End Sub
-
-    Private Sub BunifuFlatButton1_Click(sender As Object, e As EventArgs) Handles BunifuFlatButton1.Click
-
-    End Sub
 End Class
